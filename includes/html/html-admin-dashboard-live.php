11e88b357f9be97773738b40e96dce9acce72f74
Travis Kellum <travis@kellumsolutions.com>
2022-01-04 09:16:57 -0700

    added setup instructions for staging site on live site

--- includes/html/html-admin-dashboard-live.php
+++ includes/html/html-admin-dashboard-live.php
@@ -4,10 +4,17 @@
     </h1>
 
     <div style="margin: 25px 0">
-        <p>This site is being tracked by www.example.com.</p>
+        <p>This site is currently not being tracked.</p>
+        <p>Start tracking changes from this site to a staging site now.</p>
+        <ol>
+            <li>Duplicate your current site to a staging site.</li>
+            <li>Install SyncEasyWP on your staging site.</li>
+            <li>Set your "Live Site" to <b class="text-success"><?= site_url(); ?></b> and "License Key" to <b class="text-success"><?= $license_key; ?></b>.</li>
+            <li>Start tracking your database changes!</li>
+        </ol>
         <br>
-        <button type="button" class="btn btn-success" style="min-width:250px">
+        <!-- <button type="button" class="btn btn-success" style="min-width:250px">
             <a href="" style="color:#ffffff;text-decoration:none;display:block">Edit Site Settings</a>
-        </button>
+        </button> -->
     </div>
 </div>
